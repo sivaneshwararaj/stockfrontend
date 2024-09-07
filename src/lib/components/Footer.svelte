0b90b5b48276bcb5205d68b15cf6ca797d849130
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-09-07 20:05:41 +0200

    update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -14,9 +14,6 @@
       <div class="flex-1 mb-8">
         <div class="flex flex-row items-center mt-4">
 
-          <a href="https://www.ko-fi.com/stocknear" rel="noopener noreferrer" target="_blank" class="flex flex-row items-center">
-            <svg class="w-5 h-5 ml-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M23.881 8.948c-.773-4.085-4.859-4.593-4.859-4.593H.723c-.604 0-.679.798-.679.798s-.082 7.324-.022 11.822c.164 2.424 2.586 2.672 2.586 2.672s8.267-.023 11.966-.049c2.438-.426 2.683-2.566 2.658-3.734c4.352.24 7.422-2.831 6.649-6.916m-11.062 3.511c-1.246 1.453-4.011 3.976-4.011 3.976s-.121.119-.31.023c-.076-.057-.108-.09-.108-.09c-.443-.441-3.368-3.049-4.034-3.954c-.709-.965-1.041-2.7-.091-3.71c.951-1.01 3.005-1.086 4.363.407c0 0 1.565-1.782 3.468-.963s1.832 3.011.723 4.311m6.173.478c-.928.116-1.682.028-1.682.028V7.284h1.77s1.971.551 1.971 2.638c0 1.913-.985 2.667-2.059 3.015"/></svg>
-          </a>
 
           <a href={discordURL} rel="noopener noreferrer" target="_blank" class="flex flex-row items-center">
             <svg class="w-6 h-6 ml-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="white" d="M19.27 5.33C17.94 4.71 16.5 4.26 15 4a.09.09 0 0 0-.07.03c-.18.33-.39.76-.53 1.09a16.09 16.09 0 0 0-4.8 0c-.14-.34-.35-.76-.54-1.09c-.01-.02-.04-.03-.07-.03c-1.5.26-2.93.71-4.27 1.33c-.01 0-.02.01-.03.02c-2.72 4.07-3.47 8.03-3.1 11.95c0 .02.01.04.03.05c1.8 1.32 3.53 2.12 5.24 2.65c.03.01.06 0 .07-.02c.4-.55.76-1.13 1.07-1.74c.02-.04 0-.08-.04-.09c-.57-.22-1.11-.48-1.64-.78c-.04-.02-.04-.08-.01-.11c.11-.08.22-.17.33-.25c.02-.02.05-.02.07-.01c3.44 1.57 7.15 1.57 10.55 0c.02-.01.05-.01.07.01c.11.09.22.17.33.26c.04.03.04.09-.01.11c-.52.31-1.07.56-1.64.78c-.04.01-.05.06-.04.09c.32.61.68 1.19 1.07 1.74c.03.01.06.02.09.01c1.72-.53 3.45-1.33 5.25-2.65c.02-.01.03-.03.03-.05c.44-4.53-.73-8.46-3.1-11.95c-.01-.01-.02-.02-.04-.02zM8.52 14.91c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.84 2.12-1.89 2.12zm6.97 0c-1.03 0-1.89-.95-1.89-2.12s.84-2.12 1.89-2.12c1.06 0 1.9.96 1.89 2.12c0 1.17-.83 2.12-1.89 2.12z"/></svg>
@@ -36,7 +33,7 @@
 
         <!--
         <a href="https://stats.uptimerobot.com/MP0zLix9yk" rel="noopener noreferrer" target="_blank" class="flex flex-row items-center mt-4">
-          <span class="text-white sm:hover:text-purple-400">
+          <span class="text-white sm:hover:text-blue-400">
             Server Status
           </span>
           <span class="relative flex h-3 w-3 ml-2">
@@ -52,13 +49,12 @@
         <p class="uppercase text-slate-100 md:mb-6">Products</p>
         <ul class="list-reset mb-6">
           <li class="mt-2 inline-block mr-2 md:block md:mr-0" >
-            <a href="/pricing" class="no-underline hover:underline text-gray-300  sm:hover:text-purple-400">Pricing</a>
+            <a href="/pricing" class="no-underline sm:hover:underline text-gray-100  sm:hover:text-blue-400">Pricing</a>
           </li>
-          <!--
+          
           <li class="mt-2 inline-block mr-2 md:block md:mr-0" >
-            <a href="/changelog" class="no-underline hover:underline text-gray-300  sm:hover:text-purple-400">Changelog</a>
+            <a rel="noopener noreferrer" target="_blank" href="https://www.ko-fi.com/stocknear" class="no-underline sm:hover:underline text-gray-100  sm:hover:text-blue-400">Donation ❤</a>
           </li>
-          -->
         </ul>
       </div>
 
@@ -66,13 +62,13 @@
         <p class="uppercase text-slate-100 md:mb-6">Legal</p>
         <ul class="list-reset mb-6">
           <li class="mt-2 inline-block mr-2 md:block md:mr-0" >
-            <a href="/terms-of-use" class="no-underline hover:underline text-gray-300  sm:hover:text-purple-400">Terms of Use</a>
+            <a href="/terms-of-use" class="no-underline sm:hover:underline text-gray-100  sm:hover:text-blue-400">Terms of Use</a>
           </li>
           <li class="mt-2 inline-block mr-2 md:block md:mr-0" >
-            <a href="/privacy-policy" class="no-underline hover:underline text-gray-300  sm:hover:text-purple-400">Privacy Policy</a>
+            <a href="/privacy-policy" class="no-underline sm:hover:underline text-gray-100  sm:hover:text-blue-400">Privacy Policy</a>
           </li>
           <li class="mt-2 inline-block mr-2 md:block md:mr-0" >
-            <a href="/imprint" class="no-underline hover:underline text-gray-300  sm:hover:text-purple-400">Imprint</a>
+            <a href="/imprint" class="no-underline sm:hover:underline text-gray-100  sm:hover:text-blue-400">Imprint</a>
           </li>
         </ul>
       </div>
@@ -82,23 +78,23 @@
         <ul class="list-reset mb-6">
 
           <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="/about" class="no-underline hover:underline text-gray-300 sm:hover:text-purple-400">
+            <a href="/about" class="no-underline sm:hover:underline text-gray-100 sm:hover:text-blue-400">
               About Us
             </a>
           </li>
           <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="/contact" class="no-underline hover:underline text-gray-300 sm:hover:text-purple-400">
+            <a href="/contact" class="no-underline sm:hover:underline text-gray-100 sm:hover:text-blue-400">
               Contact Us
             </a>
           </li>
           <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="/blog" class="no-underline hover:underline text-gray-300 sm:hover:text-purple-400">
+            <a href="/blog" class="no-underline sm:hover:underline text-gray-100 sm:hover:text-blue-400">
               Blog
             </a>
           </li>
           <!--
           <li class="mt-2 inline-block mr-2 md:block md:mr-0">
-            <a href="/newsletter" class="no-underline hover:underline text-gray-300 sm:hover:text-purple-400">
+            <a href="/newsletter" class="no-underline sm:hover:underline text-gray-100 sm:hover:text-blue-400">
               Newsletter
             </a>
           </li>
