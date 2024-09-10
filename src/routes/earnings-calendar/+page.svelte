179a619d01d28b6d8e66bd9b7e995a7f06aa8b68
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-09-10 22:07:38 +0200

    ui fixes

--- src/routes/earnings-calendar/+page.svelte
+++ src/routes/earnings-calendar/+page.svelte
@@ -472,30 +472,30 @@ $: {
               </div>
               {/if}
 
-              <div on:click={() => goto('/analysts')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
+              <div on:click={() => goto('/dividends-calendar')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
                   <div class="w-auto lg:w-full p-1 flex flex-col m-auto px-2 sm:px-0">
                       <div class="w-full flex justify-between items-center p-3 mt-3">
                       <h2 class="text-start text-xl font-semibold text-white ml-3">
-                      Wallstreet Analyst
+                      Dividend Calendar 💸
                       </h2>
                       <ArrowLogo class="w-8 h-8 mr-3 flex-shrink-0"/>
                       </div>
                       <span class="text-white p-3 ml-3 mr-3">
-                          Get the latest top Wall Street analyst ratings.
+                          Stay updated on upcoming Dividends in the stock market.
                       </span>
                   </div>
               </div>
 
-              <div on:click={() => goto('/politicians')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
+              <div on:click={() => goto('/economic-calendar')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
                   <div class="w-auto lg:w-full p-1 flex flex-col m-auto px-2 sm:px-0">
                       <div class="w-full flex justify-between items-center p-3 mt-3">
                       <h2 class="text-start text-xl font-semibold text-white ml-3">
-                      Congress Trading
+                      Economic Events 🌍
                       </h2>
                       <ArrowLogo class="w-8 h-8 mr-3 flex-shrink-0"/>
                       </div>
                       <span class="text-white p-3 ml-3 mr-3">
-                          Get the latest top Congress trading insights.
+                          Stay updated on upcoming Economic Events worldwide.
                       </span>
                   </div>
               </div>
--- src/routes/economic-calendar/+page.svelte
+++ src/routes/economic-calendar/+page.svelte
@@ -4,6 +4,7 @@
   import logo from '$lib/images/transcripts_logo.png';
   import { listOfCountries } from '$lib/utils';
   import ArrowLogo from "lucide-svelte/icons/move-up-right";
+  import { goto } from '$app/navigation';
 
   export let data;
   let rawData;
@@ -392,30 +393,30 @@
         </div>
         {/if}
 
-        <div on:click={() => goto('/analysts')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
+        <div on:click={() => goto('/earnings-calendar')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
             <div class="w-auto lg:w-full p-1 flex flex-col m-auto px-2 sm:px-0">
                 <div class="w-full flex justify-between items-center p-3 mt-3">
                 <h2 class="text-start text-xl font-semibold text-white ml-3">
-                Wallstreet Analyst
+                Earnings Calendar 🌟
                 </h2>
                 <ArrowLogo class="w-8 h-8 mr-3 flex-shrink-0"/>
                 </div>
                 <span class="text-white p-3 ml-3 mr-3">
-                    Get the latest top Wall Street analyst ratings.
+                    Get the latest Earnings of companies
                 </span>
             </div>
         </div>
 
-        <div on:click={() => goto('/politicians')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
+        <div on:click={() => goto('/dividends-calendar')} class="w-full bg-[#141417] duration-100 ease-out sm:hover:text-white text-gray-400 sm:hover:border-gray-700 border border-gray-800 rounded-lg h-fit pb-4 mt-4 cursor-pointer">
             <div class="w-auto lg:w-full p-1 flex flex-col m-auto px-2 sm:px-0">
                 <div class="w-full flex justify-between items-center p-3 mt-3">
                 <h2 class="text-start text-xl font-semibold text-white ml-3">
-                Congress Trading
+                Dividend Calendar 💸
                 </h2>
                 <ArrowLogo class="w-8 h-8 mr-3 flex-shrink-0"/>
                 </div>
                 <span class="text-white p-3 ml-3 mr-3">
-                    Get the latest top Congress trading insights.
+                    Get the latest dividend announcement
                 </span>
             </div>
         </div>
