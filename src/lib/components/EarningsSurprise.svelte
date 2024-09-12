4b2b3b5c0e69897c5350fbee4be0601988303779
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-09-12 21:46:24 +0200

    correct text

--- src/lib/components/EarningsSurprise.svelte
+++ src/lib/components/EarningsSurprise.svelte
@@ -20,7 +20,7 @@ function latestInfoDate(inputDate) {
     const differenceInDays = Math?.floor(differenceInMs / (1000 * 60 * 60 * 24));
 
     // Return the difference in days
-    return differenceInDays <=2;
+    return differenceInDays <=1;
 }
 
 
@@ -57,7 +57,7 @@ $: {
         </div>
 
     {#if data?.user?.tier === 'Pro'}
-        <div class="text-white text-[1rem] {latestInfoDate(rawData?.date) ? 'bg-[#F9AB00] bg-opacity-[0.1] p-3 rounded-lg' : 'bg-[#27272A]'} ">
+        <div class="text-white text-[1rem] {latestInfoDate(rawData?.date) ? 'bg-[#F9AB00] bg-opacity-[0.1] p-3 rounded-lg' : 'bg-[#09090B] pl-3'} ">
             <div class="mt-1">{$displayCompanyName} has released their quartely earnings on {new Date(rawData?.date)?.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric', daySuffix: '2-digit' })}:</div>
   
                     <li class="ml-[20px] sm:ml-[30px]" style="color: #fff; line-height: 22px; margin-top:20px; margin-bottom: 15px; list-style-type: disc;">
