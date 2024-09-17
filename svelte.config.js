8d8c72881212d148534180f9c45deb837f535e86
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-09-17 23:44:34 +0200

    switch completely to verce function instead edge function

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,24 @@
 //import adapter from '@sveltejs/adapter-static';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from "@sveltejs/adapter-vercel";
 
-import compression from 'compression';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import compression from "compression";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			// Add the compression middleware to the adapter options
-			middleware: (handler) => compression()(handler),
-			runtime: 'edge',
-			regions: ['fra1'],
-			worker: true, // Add this line if the adapter supports handling worker files
-		}),
-	},
-	preprocess: vitePreprocess(),
-	paths: {
-		relative: false,
-	  },
-	
+  kit: {
+    adapter: adapter({
+      // Add the compression middleware to the adapter options
+      middleware: (handler) => compression()(handler),
+      runtime: "nodejs20.x",
+      regions: ["fra1"],
+      worker: true, // Add this line if the adapter supports handling worker files
+    }),
+  },
+  preprocess: vitePreprocess(),
+  paths: {
+    relative: false,
+  },
 };
 
 export default config;
