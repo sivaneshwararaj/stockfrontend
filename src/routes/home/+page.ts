53b7933ac890d10549380db230b36e81fc8c77aa
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-09-18 12:52:33 +0200

    redirect

--- src/routes/home/+page.ts
+++ src/routes/home/+page.ts
@@ -1,39 +1,5 @@
-import { getCache, setCache } from '$lib/store';
-import { redirect } from '@sveltejs/kit';
+import { redirect } from "@sveltejs/kit";
 
-
-export const load = async ({parent}) => {
-
-  const { user, apiKey, apiURL } = await parent();
-
-
-  if (!user) {
-		redirect(303, '/');
-	}
-
-  const getDashboard = async () => {
-
-    // Get cached data for the specific tickerID
-    const cachedData = getCache('', 'getDashboard');
-    if (cachedData) {
-      return cachedData;
-    } else {
-
-      const response = await fetch(apiURL + '/dashboard-info', {
-        method: 'GET',
-        headers: {
-          "Content-Type": "application/json", "X-API-KEY": apiKey
-        },
-      });
-
-      const output = await response?.json();
-      setCache('', output, 'getDashboard');
-      return output;
-    }
-  };
-
-  // Make sure to return a promise
-  return {
-    getDashboard: await getDashboard(),
-  };
+export const load = async () => {
+  redirect(303, "/");
 };
