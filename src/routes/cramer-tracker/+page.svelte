6bb8ca08725b74b4d74c947e7bea306d3b6338fb
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-11-05 11:18:37 +0100

    bugfixing

--- src/routes/cramer-tracker/+page.svelte
+++ src/routes/cramer-tracker/+page.svelte
@@ -3,6 +3,7 @@
   import { onMount } from "svelte";
   import ArrowLogo from "lucide-svelte/icons/move-up-right";
   import HoverStockChart from "$lib/components/HoverStockChart.svelte";
+  import TableHeader from "$lib/components/Table/TableHeader.svelte";
 
   export let data;
 
@@ -15,43 +16,43 @@
     let path;
     switch (sector) {
       case "Financials":
-        path = "financial-sector";
+        path = "financial";
         break;
       case "Healthcare":
-        path = "healthcare-sector";
+        path = "healthcare";
         break;
       case "Information Technology":
-        path = "technology-sector";
+        path = "technology";
         break;
       case "Technology":
-        path = "technology-sector";
+        path = "technology";
         break;
       case "Financial Services":
-        path = "financial-sector";
+        path = "financial";
         break;
       case "Industrials":
-        path = "industrials-sector";
+        path = "industrials";
         break;
       case "Energy":
-        path = "energy-sector";
+        path = "energy";
         break;
       case "Utilities":
-        path = "utilities-sector";
+        path = "utilities";
         break;
       case "Consumer Cyclical":
-        path = "consumer-cyclical-sector";
+        path = "consumer-cyclical";
         break;
       case "Real Estate":
-        path = "real-estate-sector";
+        path = "real-estate";
         break;
       case "Basic Materials":
-        path = "basic-materials-sector";
+        path = "basic-materials";
         break;
       case "Communication Services":
-        path = "communication-services-sector";
+        path = "communication-services";
         break;
       case "Consumer Defensive":
-        path = "consumer-defensive-sector";
+        path = "consumer-defensive";
         break;
       default:
         // Handle default case if needed
@@ -117,6 +118,82 @@
   });
 
   $: charNumber = 20;
+
+  let columns = [
+    { key: "date", label: "Date", align: "left" },
+    { key: "ticker", label: "Symbol", align: "left" },
+    { key: "name", label: "Name", align: "left" },
+    { key: "sentiment", label: "Sentiment", align: "right" },
+    { key: "returnSince", label: "Return Since", align: "right" },
+    { key: "sector", label: "Sector", align: "right" },
+  ];
+
+  let sortOrders = {
+    date: { order: "none", type: "date" },
+    ticker: { order: "none", type: "string" },
+    name: { order: "none", type: "string" },
+    sentiment: { order: "none", type: "string" },
+    returnSince: { order: "none", type: "number" },
+    sector: { order: "none", type: "string" },
+  };
+
+  const sortData = (key) => {
+    // Reset all other keys to 'none' except the current key
+    for (const k in sortOrders) {
+      if (k !== key) {
+        sortOrders[k].order = "none";
+      }
+    }
+
+    // Cycle through 'none', 'asc', 'desc' for the clicked key
+    const orderCycle = ["none", "asc", "desc"];
+
+    let originalData = rawData;
+
+    const currentOrderIndex = orderCycle.indexOf(sortOrders[key].order);
+    sortOrders[key].order =
+      orderCycle[(currentOrderIndex + 1) % orderCycle.length];
+    const sortOrder = sortOrders[key].order;
+
+    // Reset to original data when 'none' and stop further sorting
+    if (sortOrder === "none") {
+      displayList = [...originalData]?.slice(0, 50); // Reset to original data (spread to avoid mutation)
+      return;
+    }
+
+    // Define a generic comparison function
+    const compareValues = (a, b) => {
+      const { type } = sortOrders[key];
+      let valueA, valueB;
+
+      switch (type) {
+        case "date":
+          valueA = new Date(a[key]);
+          valueB = new Date(b[key]);
+          break;
+        case "string":
+          valueA = a[key].toUpperCase();
+          valueB = b[key].toUpperCase();
+          return sortOrder === "asc"
+            ? valueA.localeCompare(valueB)
+            : valueB.localeCompare(valueA);
+        case "number":
+        default:
+          valueA = parseFloat(a[key]);
+          valueB = parseFloat(b[key]);
+          break;
+      }
+
+      if (sortOrder === "asc") {
+        return valueA < valueB ? -1 : valueA > valueB ? 1 : 0;
+      } else {
+        return valueA > valueB ? -1 : valueA < valueB ? 1 : 0;
+      }
+    };
+
+    // Sort using the generic comparison function
+    displayList = [...originalData].sort(compareValues)?.slice(0, 50);
+  };
 </script>
 
 <svelte:head>
@@ -199,98 +276,21 @@
               </span>
             </div>
 
-            <div class="w-screen sm:w-full m-auto mt-20 sm:mt-10">
+            <div class="w-full m-auto mt-20 sm:mt-10">
               <div
-                class="w-screen sm:w-full m-auto rounded-none sm:rounded-lg mb-4 overflow-x-scroll"
+                class="w-full m-auto rounded-none sm:rounded-lg mb-4 overflow-x-scroll"
               >
                 <table
                   class="table table-sm table-compact no-scrollbar rounded-none sm:rounded-md w-full bg-[#09090B] border-bg-[#09090B] m-auto"
                 >
                   <thead>
-                    <tr class="bg-[#09090B] border-b border-[#27272A]">
-                      <th
-                        class="text-start bg-[#09090B] text-white text-sm sm:text-[1rem] font-semibold"
-                      >
-                        Company Name
-                      </th>
-                      <th
-                        class="text-start bg-[#09090B] text-white text-sm sm:text-[1rem] font-semibold"
-                      >
-                        Date
-                      </th>
-                      <th
-                        class="text-center bg-[#09090B] text-white text-sm sm:text-[1rem] font-semibold"
-                      >
-                        Sentiment
-                      </th>
-                      <th
-                        class="text-end bg-[#09090B] text-white text-sm sm:text-[1rem] font-semibold"
-                      >
-                        Return Since
-                      </th>
-                      <th
-                        class=" text-end bg-[#09090B] text-white text-sm sm:text-[1rem] font-semibold"
-                      >
-                        Sector
-                      </th>
-                    </tr>
+                    <TableHeader {columns} {sortOrders} {sortData} />
                   </thead>
                   <tbody>
-                    {#each displayList as item, index}
+                    {#each displayList as item}
                       <tr
                         class="sm:hover:bg-[#245073] border-b border-[#27272A] sm:hover:bg-opacity-[0.2] odd:bg-[#27272A]"
                       >
-                        <td
-                          class="text-sm sm:text-[1rem] text-start whitespace-nowrap"
-                        >
-                          {#if index >= 5 && data?.user?.tier !== "Pro"}
-                            <a class="block relative" href="/pricing">
-                              <span
-                                class="text-base font-semibold text-blue-link blur group-hover:blur-[6px]"
-                              >
-                                XXXX
-                              </span>
-
-                              <div
-                                class="ml-px max-w-[130px] truncate text-sm text-default blur group-hover:blur-[6px] lg:max-w-[150px]"
-                              >
-                                XXXXXXXXXXXXXXXX
-                              </div>
-
-                              <div class="absolute top-3 flex items-center">
-                                <svg
-                                  class="size-5 text-[#FBCE3C]"
-                                  viewBox="0 0 20 20"
-                                  fill="currentColor"
-                                  style="max-width: 40px;"
-                                >
-                                  <path
-                                    fill-rule="evenodd"
-                                    d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                                    clip-rule="evenodd"
-                                  >
-                                  </path>
-                                </svg>
-
-                                <span
-                                  class="ml-1 font-semibold text-muted group-hover:text-default"
-                                >
-                                  Upgrade
-                                </span>
-                              </div>
-                            </a>
-                          {:else}
-                            <div class="flex flex-col items-start">
-                              <HoverStockChart symbol={item?.ticker} />
-                              <span class="text-white">
-                                {item?.name?.length > charNumber
-                                  ? item?.name?.slice(0, charNumber) + "..."
-                                  : item?.name}
-                              </span>
-                            </div>
-                          {/if}
-                        </td>
-
                         <td
                           class="text-start text-sm sm:text-[1rem] whitespace-nowrap text-white"
                         >
@@ -300,6 +300,20 @@
                             year: "numeric",
                             daySuffix: "2-digit",
                           })}
+                        </td>
+
+                        <td
+                          class="text-sm sm:text-[1rem] text-start whitespace-nowrap"
+                        >
+                          <HoverStockChart symbol={item?.ticker} />
+                        </td>
+
+                        <td
+                          class="text-start text-sm sm:text-[1rem] whitespace-nowrap text-white"
+                        >
+                          {item?.name?.length > charNumber
+                            ? item?.name?.slice(0, charNumber) + "..."
+                            : item?.name}
                         </td>
 
                         <td
@@ -314,7 +328,7 @@
                                 ) ||
                                 item?.sentiment?.includes('Sell') === 'Bearish'
                               ? 'text-[#FF2F1F]'
-                              : 'text-[#C6A755]'} text-center"
+                              : 'text-[#C6A755]'} text-end"
                         >
                           {item?.sentiment}
                         </td>
@@ -331,7 +345,8 @@
                           class="text-end text-sm sm:text-[1rem] whitespace-nowrap font-medium"
                         >
                           <a
-                            href={"/list/" + sectorSelector(item?.sector)}
+                            href={"/list/sector/" +
+                              sectorSelector(item?.sector)}
                             class="sm:hover:text-white text-blue-400"
                           >
                             {item?.sector}
