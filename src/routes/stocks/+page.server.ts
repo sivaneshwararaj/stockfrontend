ac175c037f4334901a16ec4d895a3b2a12fb0d9f
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-11-16 23:43:04 +0100

    refactor all stocks page

--- src/routes/stocks/+page.server.ts
+++ src/routes/stocks/+page.server.ts
@@ -1,14 +1,15 @@
 export const load = async ({ locals }) => {
   const getStockList = async () => {
     const { apiKey, apiURL } = locals;
-
+    const postData = {'filterList': 'all-stock-tickers'}
     // make the POST request to the endpoint
-    const response = await fetch(apiURL + "/all-stock-tickers", {
-      method: "GET",
+    const response = await fetch(apiURL + "/list-category", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
         "X-API-KEY": apiKey,
       },
+      body: JSON.stringify(postData)
     });
 
     const output = await response.json();
