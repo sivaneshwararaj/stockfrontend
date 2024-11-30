eaee3cf33da3400e70833c5b83cab1030342eb38
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-11-30 12:25:27 +0100

    testing pixel code

--- src/routes/pricing/+page.svelte
+++ src/routes/pricing/+page.svelte
@@ -41,14 +41,23 @@
   async function purchasePlan(subscriptionType: string = "") {
     if (data?.user) {
       let subId = "";
-
+      let value = 0;
       if (subscriptionType === "lifeTime") {
         subId = import.meta.env.VITE_LEMON_SQUEEZY_LIFE_TIME_ACCESS_ID;
+        value = 599;
       } else if (mode) {
         subId = import.meta.env.VITE_LEMON_SQUEEZY_ANNUAL_ID;
+        value = 59.88;
       } else {
         subId = import.meta.env.VITE_LEMON_SQUEEZY_MONTHLY_ID;
+        value = 9.99;
       }
+
+      twq("event", "tw-onuuu-ospg6", {
+        value: value,
+        currency: "USD",
+        conversion_id: data?.user?.id,
+      });
 
       const isDarkMode =
         window.matchMedia &&
@@ -104,6 +113,24 @@
     window.lemonSqueezyAffiliateConfig = { store: "stocknear" };
   </script>
   <script src="https://lmsqueezy.com/affiliate.js" defer></script>
+
+  <script>
+    !(function (e, t, n, s, u, a) {
+      e.twq ||
+        ((s = e.twq =
+          function () {
+            s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments);
+          }),
+        (s.version = "1.1"),
+        (s.queue = []),
+        (u = t.createElement(n)),
+        (u.async = !0),
+        (u.src = "https://static.ads-twitter.com/uwt.js"),
+        (a = t.getElementsByTagName(n)[0]),
+        a.parentNode.insertBefore(u, a));
+    })(window, document, "script");
+    twq("config", "onuuu");
+  </script>
 </svelte:head>
 
 <section
