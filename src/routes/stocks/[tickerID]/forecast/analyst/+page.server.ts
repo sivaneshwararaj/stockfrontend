a8955eb30747ed5f8141d66a8afb4fe54f310c0e
MuslemRahimi <moslem_rahimi@hotmail.de>
2024-12-11 13:24:32 +0100

    login fixes

--- src/routes/stocks/[tickerID]/forecast/analyst/+page.server.ts
+++ src/routes/stocks/[tickerID]/forecast/analyst/+page.server.ts
@@ -3,38 +3,6 @@ import { validateData } from "$lib/utils";
 import { loginUserSchema, registerUserSchema } from "$lib/schemas";
 
 
-export const load = async ({ params, locals }) => {
-  const getStockDividend = async () => {
-    let newsList;
-
-    const { apiURL, apiKey } = locals;
-
-    const postData = {
-      ticker: params.tickerID,
-    };
-
-    // make the POST request to the endpoint
-    const response = await fetch(apiURL + "/stock-dividend", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "X-API-KEY": apiKey,
-      },
-      body: JSON.stringify(postData),
-    });
-
-    newsList = await response.json();
-
-    return newsList;
-  };
-
-  // Make sure to return a promise
-  return {
-    getStockDividend: await getStockDividend(),
-  };
-};
-
-
 
 export const actions = {
   login: async ({ url, request, locals }) => {
