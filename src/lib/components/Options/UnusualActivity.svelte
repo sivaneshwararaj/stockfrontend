fc4ecd47ba7abea45719c5c02cd8d4a9f332e49d
MuslemRahimi <moslem_rahimi@hotmail.de>
2025-01-24 17:42:11 +0100

    ui fix

--- src/lib/components/Options/UnusualActivity.svelte
+++ src/lib/components/Options/UnusualActivity.svelte
@@ -347,7 +347,7 @@
     >
       <div class="sm:pl-7 sm:pb-7 sm:pt-7 w-full m-auto mt-2 sm:mt-0">
         <h2
-          class=" flex flex-row items-center text-white text-xl sm:text-2xl font-bold w-fit"
+          class=" flex flex-row items-center text-white text-xl sm:text-2xl font-bold w-fit mb-2 sm:mb-0"
         >
           Unusual Activity
         </h2>
